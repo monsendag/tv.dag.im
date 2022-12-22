26026b6bd3d4ed1941b5c72b49bfa39b15bd252a
Dag Einar Monsen <dag.einar.monsen@vegvesen.no>
2022-12-22 18:29:02 +0100

    build docs

---
+++ docs/_app/immutable/chunks/2-af3a0ed6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2d05ef30.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "",
+        },
+    }
 };
 
 export default config;
