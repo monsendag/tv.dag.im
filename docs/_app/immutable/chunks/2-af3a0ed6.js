d4d3611def2cc5db256368da6dcc0fe0fab84ddb
Dag Einar Monsen <me@dag.im>
2023-01-12 17:53:15 +0100

    update

--- docs/_app/immutable/chunks/2-af3a0ed6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-2d05ef30.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,21 @@
 <h1>Streaming sites</h1>
 
 <ul>
-    <li><a href="https://twe.no">TWE</a></li>
-    <li><a href="https://tv.nrk.no">NRK</a></li>
-    <li><a href="https://www.hbomax.com">HBO</a></li>
-    <li><a href="https://www.netflix.com">Netflix</a></li>
-    <li><a href="https://www.plex.tv">Plex</a></li>
-    <li><a href="https://www.youtube.com">Youtube</a></li>
+	<li><a href="https://twe.no">TWE</a></li>
+	<li><a href="https://tv.nrk.no">NRK</a></li>
+	<li><a href="https://www.hbomax.com">HBO</a></li>
+	<li><a href="https://app.plex.tv">Plex</a></li>
 </ul>
 
 <style>
-    h1 {
-        text-align: center;
-    }
+	h1 {
+		text-align: center;
+	}
 
-    ul {
-        list-style-type: none;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-    }
+	ul {
+		font-size: 5rem;
+		list-style-type: none;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
 </style>
