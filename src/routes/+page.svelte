0c13ae33f3b1660466bc12fc26a09a6fece4206e
Dag Einar Monsen <me@dag.im>
2023-08-29 21:18:49 +0200

    switch to logos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,39 +3,47 @@
 	const sites = [
 		{
 			title: "NRK TV",
-			url: "https://tv.nrk.no"
+			url: "https://tv.nrk.no",
+			image: 'nrk-tv.webp'
 		},
 		{
 			title: "TV2 Play",
-			url: "https://play.tv2.no"
+			url: "https://play.tv2.no",
+			image: 'tv2-play.webp'
 		},
 		{
 			title: "T-WE",
-			url: "https://twe.no"
+			url: "https://twe.no",
+			image: "t-we.webp"
 		},
 		{
 			title: "HBO MAX",
-			url: "https://play.hbomax.com"
+			url: "https://play.hbomax.com",
+			image: "hbo-max.webp"
 		},
 		{
 			title: "Plex",
-			url: "https://app.plex.tv"
+			url: "https://app.plex.tv",
+			image: "plex.webp"
 		},
 
 	]
 
 </script>
-<h1>Streaming sites</h1>
 
 <ul>
 	{#each sites as site}
-		<li><a href="https://www.youtube.com/redirect?q={site.url}">{site.title}</a></li>
+		<li><a href="https://www.youtube.com/redirect?q={site.url}"><img width=256 src="{site.image}" alt="{site.title}" /></a></li>
 	{/each}
 </ul>
 
 <style>
 	h1 {
 		text-align: center;
+	}
+
+	img {
+		border-radius: 20px;
 	}
 
 	ul {
