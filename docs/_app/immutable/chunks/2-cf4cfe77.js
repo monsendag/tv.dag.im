6a19cd485add1eb09bfefbe6ad1c997dee8320c2
Dag Einar Monsen <me@dag.im>
2023-08-29 20:46:19 +0200

    upgrade svelte and switch to github workflow

--- docs/_app/immutable/chunks/2-cf4cfe77.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b8d9a805.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
-// was "@sveltejs/adapter-auto"
-
-const dev = "production" === "development";
 
 /** @type {import(""@sveltejs/kit").Config} */
 const config = {
     kit: {
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            // change below to your repo name
-            base: dev ? "" : "",
-        },
+        adapter: adapter(),
     }
 };
 
